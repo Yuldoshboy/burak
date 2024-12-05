c8ce24960a80b52b97cc5a48eca502af75561588
Yuldoshboy <sobirov.yu@gmail.com>
2024-12-05 12:04:31 +0900

    feat: design patterns has been added

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,13 @@
 
 // const person: string = "Martin";
 // let age: number = 25;
+
+//PATTERNS
+
+//Architectural pattern: MVC (Model View Controller), DI (Dependency Injection), MVP
+
+//BACKEND: asosiy suyagi, malumotlar oqimini nazorat qiladi
+
+//Design pettern: Middleware, Decorator
+// Backendni malum bir qismini nazorat qiladigan pattern
+// Creational Design Pattern, Structural Design Pattern, Behavioural Design Pattern
