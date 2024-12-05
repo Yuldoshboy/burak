add8d4e89eb476946409dd8c8f154ae11b09b72c
Yuldoshboy <sobirov.yu@gmail.com>
2024-12-05 11:37:45 +0900

    BRR: Initial setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// console.log("Executed!");
+
+// import moment from "moment";
+// const currentTime = moment().format("YYYY MM DD HH MM SS");
+// console.log(currentTime);
+
+// const person: string = "Martin";
+// let age: number = 25;
